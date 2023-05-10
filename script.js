0776de7c9c99b7dbbfe89e45cbe5317452497760
John Kersey <johnkersey2@yahoo.com>
2023-05-10 10:46:02 -0400

    Added main functions. Still debugging

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Adds event listener to save user input
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $('.saveBtn').on('click', function(){
+    var timeBlock = parseInt($(this).parent().attr('id'))
+    var task = $(this).parent().children().eq(1).val()
+    localStorage.setItem(timeBlock, task)
+  })
+
+  // Calls the time block class and gives a function for each div containing this class
+  $('.time-block').each(function(){
+    var currentHour = dayjs().hour();
+    var hour = parseInt($(this).attr('id'));
+
+    // applies "past" class
+       if (hour < currentHour){
+      $(this).addClass('past');
+      $(this).removeClass('present');
+      $(this).removeClass('future');
+   // applies "future" class
+    } else if (hour > currentHour){
+      $(this).addClass('future');
+      $(this).removeClass('present');
+      $(this).removeClass('past');
+   // applies "present" class
+    } else {
+      $(this).addClass('present');
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+    }
+  })
+
+  // calls the 'description' class
+  $('.description').each(function(){
+    var timeBlock = parseInt($(this).parent().attr('id'))
+    var description = localStorage.getItem(timeBlock)
+    $(this).text(description)
+  })
+
+  // Gets the current day and displays it in the header
+  var currentDay = dayjs().format('dddd, MMMM D');
+  $('#currentDay').text(currentDay);
 });
